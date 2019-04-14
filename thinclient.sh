d1b17f605ed0374100d8f0d426ccebd97ebc53c4
David Gengenbach <davidgengenbach@gmx.de>
2019-04-14 13:09:42 +0200

    Thinclient init script improvement

--- thinclient.sh
+++ thinclient.sh
@@ -4,7 +4,7 @@ echo "Enter hostname (without .patagona.office), followed by [ENTER]:"
 
 read new_hostname
 
-echo "Are you sure you want to use hostname: $new_hostname?"
+echo "Are you sure you want to use hostname: $new_hostname? Press [ENTER] to continue."
 
 read xxx
 
@@ -12,14 +12,14 @@ hostnamectl set-hostname $new_hostname
 
 echo "${new_hostname}.patagona.office" > /etc/salt/minion_id
 
+echo "Adding 'thinclient' role"
+salt-call --local grains.append roles thinclient
 
-for action in start enable status; do
+for action in start enable restart status; do
   systemctl $action salt-minion
 done
 
-echo "Restart?"
-
-read -p "Are you alright? (y/n) " RESP
+read -p "Restart? (y/n) " RESP
 if [ "$RESP" = "y" ]; then
   reboot
 fi
